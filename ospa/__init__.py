d4eb94b2271cffb56a88130e05481d7669fbfd87
kwent <alekseev.yeskela@gmail.com>
2019-04-06 13:53:58 +0300

    dummy test folder

--- ospa/__init__.py
+++ ospa/__init__.py
@@ -1,5 +1,16 @@
 import os
 
+from ospa_exception import OspaException
 
-def listdir(path):
+
+def listdir(path, full_path=True, only_files=False, use_walk=False):
+    if only_files is False and use_walk is True:
+        raise OspaException('only_files is False and use_walk is True, it is not correct')
     return [os.path.join(path, x) for x in os.listdir(path)]
+
+
+if __name__ == '__main__':
+    # listdir('.', only_files=False, use_walk=True)
+    listdir('.', only_files=False, use_walk=False)
+    listdir('.', only_files=True, use_walk=True)
+    listdir('.', only_files=True, use_walk=False)
