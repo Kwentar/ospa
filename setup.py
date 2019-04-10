f357c205d2ed9c1192e5e69a2ec61db0f7fba495
kwent <alekseev.yeskela@gmail.com>
2019-04-10 23:45:41 +0300

    correct version, release

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
     setuptools.setup(
      name='ospa',
-     version='0.1.0',
+     version='0.0.1',
      author="Aleksei Alekseev",
      author_email="alekseev.yeskela@gmail.com",
      description="OS extension",
